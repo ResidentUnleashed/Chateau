9cd8f3ce395be271f38c1d820dd61e875743c3f1
ResidentUnleashed <31601762+ResidentUnleashed@users.noreply.github.com>
2018-09-03 13:46:46 +1000

    Camera move code cleanup
    Made code more efficent

--- Assets/Scripts/FixedCamera.cs
+++ Assets/Scripts/FixedCamera.cs
@@ -25,6 +25,7 @@ public class FixedCamera : MonoBehaviour {
 		if(changeCameraTarget.CanSwitchTarget)
         {
             StartCoroutine(Transition());
+            changeCameraTarget.CanSwitchTarget = false;
         }
 	}
 
