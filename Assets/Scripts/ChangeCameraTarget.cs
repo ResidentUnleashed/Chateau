e9c16fba72c871565fc98be4fa4ffc4d1a8f55e6
ResidentUnleashed <31601762+ResidentUnleashed@users.noreply.github.com>
2018-09-03 12:26:38 +1000

    Created fixed camera
    Created the components for a basic fixed camera that moves between two targets

---
+++ Assets/Scripts/ChangeCameraTarget.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCameraTarget : MonoBehaviour {
+
+    private Transform newTarget = null;
+    private bool canSwitchTarget = false;
+
+    public Transform NewTarget
+    {
+        get { return newTarget; }
+    }
+
+    public bool CanSwitchTarget
+    {
+        get { return canSwitchTarget; }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "MoveCamera")
+        {
+            //Get the child transform and we can switch target
+            newTarget = other.transform.GetChild(0).gameObject.transform;
+            canSwitchTarget = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.transform.tag == "MoveCamera")
+        {
+            canSwitchTarget = false;
+        }
+    }
+}
---
+++ Assets/Scripts/FixedCamera.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCamera : MonoBehaviour {
+
+    [Tooltip("How fast the camera moves from one position to the next.")]
+    [SerializeField]
+    private float lerpSpeed = 3.0f;
+
+
+    private ChangeCameraTarget changeCameraTarget;
+    
+
+
+	// Use this for initialization
+	void Awake ()
+    {
+        changeCameraTarget = GameObject.FindGameObjectWithTag("Player").GetComponent<ChangeCameraTarget>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(changeCameraTarget.CanSwitchTarget)
+        {
+            StartCoroutine(Transition());
+        }
+	}
+
+
+    private IEnumerator Transition()
+    {
+        //Create the float t (time)
+        float t = 0.0f;
+
+        //Get our starting pos
+        Vector3 startPos = transform.position;
+
+        while (t < 1.0f)
+        {
+            //Clock
+            t += Time.deltaTime * (Time.timeScale / lerpSpeed);
+
+            //Lerp to new destination
+            transform.position = Vector3.Lerp(startPos, changeCameraTarget.NewTarget.position, t);
+
+            //Return
+            yield return 0;
+        }
+    }
+}
