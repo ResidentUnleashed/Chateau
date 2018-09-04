68624e76f7853fbe6aeb828f71592391d8271d51
Meaghan Rattle <residentunleashed@gmail.com>
2018-09-04 15:17:15 +1000

    Menu setup
    Created the menu prefab. Created basic pause menu features with functions that still need to be implemented at a later date

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject pauseMenuUI;
+
+    private bool gameIsPaused = false;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetButtonDown("Start") && !gameIsPaused)
+        {
+            gameIsPaused = true;
+        }
+
+        if(gameIsPaused)
+        {
+            Pause();
+        }
+        else
+        {
+            Resume();
+        }
+		
+	}
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+
+        //Set back to normal
+        Time.timeScale = 1.0f;
+
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+
+        //Freeze the game
+        Time.timeScale = 0.0f;
+
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        //Add code here
+    }
+
+    public void Quit()
+    {
+        //Add code here
+    }
+}
