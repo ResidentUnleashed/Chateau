e4987e2d1d1b7e78a15b041090b4cdfda2ea1d6d
Meaghan Rattle <residentunleashed@gmail.com>
2018-09-04 12:02:17 +1000

    Camera
    Bug fixes

--- Assets/Scripts/ChangeCameraTarget.cs
+++ Assets/Scripts/ChangeCameraTarget.cs
@@ -41,7 +41,7 @@ public class ChangeCameraTarget : MonoBehaviour
             t += Time.deltaTime * (Time.timeScale / lerpSpeed);
 
             //Lerp to new destination
-            gameCamera.transform.position = Vector3.Lerp(startPos, newTarget.position, t);
+            gameCamera.transform.position = Vector3.MoveTowards(startPos, newTarget.position, t);
 
             //Return
             yield return 0;
