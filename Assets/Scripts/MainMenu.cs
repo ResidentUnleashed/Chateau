464d831858e4aa5fd660e0e39e81da626beb3b9e
Meaghan Rattle <residentunleashed@gmail.com>
2018-09-10 09:10:12 +1000

    Main Menu Script
    Main menu initiated

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainMenuUI;
+    [SerializeField]
+    private GameObject creditsUI;
+    [SerializeField]
+    private GameObject levelSelectUI;
+
+
+
+
+    public void BackToMenu()
+    {
+        creditsUI.SetActive(false);
+        levelSelectUI.SetActive(false);
+        mainMenuUI.SetActive(true);
+    }
+
+    public void LevelSelect()
+    {
+        creditsUI.SetActive(false);
+        mainMenuUI.SetActive(false);
+        levelSelectUI.SetActive(true);
+    }
+}
