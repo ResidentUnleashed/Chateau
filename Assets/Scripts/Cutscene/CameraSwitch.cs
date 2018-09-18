11e09dab83d0d28d27697e645f077d2474820a08
Meaghan Rattle <residentunleashed@gmail.com>
2018-09-18 16:42:24 +1000

    Start cutscene
    Added a camera script for cutscene
    Waiting on cutscene to be ready

---
+++ Assets/Scripts/Cutscene/CameraSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] cameraList;
+    [SerializeField]
+    private Animator greenPipAni;
+    [SerializeField]
+    private Animator greenPipAni2;
+
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
