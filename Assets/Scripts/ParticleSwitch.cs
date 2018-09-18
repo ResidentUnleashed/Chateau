75b102aac8fbd68e5e08f575d0a886853ed59d84
Meaghan Rattle <residentunleashed@gmail.com>
2018-09-18 12:01:25 +1000

    Particle switch
    Created a scirpt to turn on and off particles

---
+++ Assets/Scripts/ParticleSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSwitch : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] onList;
+
+    [SerializeField]
+    GameObject[] offList;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "ParticleDisplay")
+        {
+            for (int i = 0; i < onList.Length; i++)
+            {
+                onList[i].SetActive(true);
+            }
+
+            for (int c = 0; c < offList.Length; c++)
+            {
+                offList[c].SetActive(false);
+            }
+        }
+
+        if(other.transform.tag == "MoveCamera")
+        {
+            for (int i = 0; i < onList.Length; i++)
+            {
+                onList[i].SetActive(false);
+            }
+
+            for (int c = 0; c < offList.Length; c++)
+            {
+                offList[c].SetActive(false);
+            }
+        }
+    }
+}
