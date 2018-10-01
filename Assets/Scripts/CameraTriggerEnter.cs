b492e1825f7787026f02c4095a763d21e5c48932
Meaghan Rattle <residentunleashed@gmail.com>
2018-10-01 16:38:46 +1000

    Created camera trigger script
    Added behaviour so that that the camera animation plays when they enter a trigger

---
+++ Assets/Scripts/CameraTriggerEnter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTriggerEnter : MonoBehaviour {
+
+    [SerializeField]
+    private Animator animator;
+
+    private void Start()
+    {
+        animator.enabled = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "CamAni")
+        {
+            animator.enabled = true;
+        }
+    }
+}
