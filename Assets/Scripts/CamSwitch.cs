919098bc4cc9274b7ea67e4ab1089229151e9c24
Andrew <andrewchristofidisdesign@gmail.com>
2018-10-02 14:40:37 +1000

    Creating new camera script

---
+++ Assets/Scripts/CamSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour {
+
+    public GameObject vCamMain;
+    public GameObject[] vCamOther;
+
+    private bool atStart = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ("Player") && atStart)
+        {
+            vCamMain.SetActive(false);
+            atStart = false;
+            Debug.Log("YES");
+        }
+    }
+}
