6f05c5874dccf99471119304a00d95e12e00adf1
Meaghan Rattle <residentunleashed@gmail.com>
2018-10-02 11:46:08 +1000

    Spiral
    Added a spiral script for pips spiraling into the portal during cutscene

---
+++ Assets/Scripts/Cutscene/Spiral.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spiral : MonoBehaviour {
+
+
+    [Tooltip("The range needed to jump to the correct position.")]
+    [SerializeField]
+    private float range = 0.1f;
+
+    [SerializeField]
+    private float speed = 1.0f;
+
+    [SerializeField]
+    private Transform[] targets;
+
+    [SerializeField]
+    private float maxWait = 1.0f;
+
+    private float rotateSpeed = 2.0f;
+
+
+
+    private float waitTimer;
+    private bool wait = false;
+    private bool hasSwitchedPos = false;
+    private Transform currTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+       //Move to new destination
+       transform.position = Vector3.MoveTowards(transform.position, currTarget.position, speed);
+       
+       //In range
+       if (Vector3.Distance(transform.position, currTarget.position) < range)
+       {
+           //Hit target and wait
+           transform.position = currTarget.position;
+            hasSwitchedPos = false;
+       }
+
+        if (!hasSwitchedPos)
+        {
+            for (int i = 0; i < targets.Length; i++)
+            {
+                if (currTarget == targets[i])
+                {
+                    if (targets[i + 1] != null)
+                    {
+                        currTarget = targets[i + 1];
+                        hasSwitchedPos = true;
+                    }
+                }
+            }
+        }
+    }
+}
