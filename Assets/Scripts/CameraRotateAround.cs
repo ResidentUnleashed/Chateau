6b303c5530cc306979eadac1e5e54a26a1a64d50
Meaghan Rattle <residentunleashed@gmail.com>
2018-10-15 13:35:21 +1100

    Placeholder menu
    Fixed the placeholder menu

---
+++ Assets/Scripts/CameraRotateAround.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotateAround : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject target = null;
+    [SerializeField]
+    private float angle = 15.0f;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.RotateAround(target.transform.position, Vector3.down, Time.deltaTime * angle);
+	}
+}
