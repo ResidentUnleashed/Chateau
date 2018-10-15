da5a7d69cd7aca6539215d5c9883e81d07a8ecfe
Meaghan Rattle <residentunleashed@gmail.com>
2018-10-16 09:42:39 +1100

    Updated animations
    Allowed the suck into portal animation to be played when you go into a portal

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -6,18 +6,31 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour {
 
     [SerializeField]
-    private float maxTimeBetweenScenes = 2.0f;
+    private float maxTimeBetweenScenes = 1.2f;
     [SerializeField]
     private string sceneName;
+    [SerializeField]
+    private Animator playerAni = null;
+
 
 
     private bool hasHit = false;
     private float timer;
+    private PlayerMovement playerMovement;
+
+    private void Awake()
+    {
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
 
     private void Update()
     {
         if(hasHit)
         {
+            playerMovement.enabled = false;
+
+            playerAni.Play("Suck");
+
             timer += Time.deltaTime;
 
             if(timer > maxTimeBetweenScenes)
