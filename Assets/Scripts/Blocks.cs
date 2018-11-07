1b3034f0c2f22f7871f6f2615580639855d0933a
Andrew <andrewchristofidisdesign@gmail.com>
2018-11-07 11:07:37 +1100

    Seperated blocks in 6

---
+++ Assets/Scripts/Blocks.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks : MonoBehaviour {
+
+    public GameObject blocks;
+
+    public bool on;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (on)
+            {
+                blocks.SetActive(true);
+            }
+            else
+            {
+                blocks.SetActive(false);
+            }
+        }
+    }
+}
